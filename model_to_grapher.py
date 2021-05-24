ba0bc60d305f4fd3aab1c0a7436636fd38126e36
lzhani <lzhani@microsoft.com>
2021-05-24 13:02:30 +0800

    checkpoint

--- model_to_grapher.py
+++ model_to_grapher.py
@@ -22,8 +22,8 @@ parser.add_argument('-o', '--output_path', type=str, required=False)
 args = parser.parse_args()
 
 if __name__ == '__main__':
-    #converter(args.input_model, args.output_path)
-    from glob import glob 
-    pbs=glob("data/testmodels/**.pb")
-    for pb in pbs:
-        converter(pb,pb.replace(".pb",".json"))
+    converter(args.input_model, args.output_path)
+    #from glob import glob 
+    #pbs=glob("data/testmodels/**.pb")
+    #for pb in pbs:
+        #converter(pb,pb.replace(".pb",".json"))
