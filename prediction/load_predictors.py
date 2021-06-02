9446d4189bace0f8c5bb5a8044bcf4b65e979884
lzhani <lzhani@microsoft.com>
2021-06-02 15:16:48 +0800

    fix download

--- prediction/load_predictors.py
+++ prediction/load_predictors.py
@@ -1,5 +1,10 @@
 import pickle,os
 from glob import glob
+import shutil
+from zipfile import ZipFile
+from tqdm import tqdm
+import requests
+
 import zipfile, requests, shutil
 def load_lat_predictors(configs,dir="data/predictorzoo"):
     hardware=configs['name']
@@ -7,15 +12,14 @@ def load_lat_predictors(configs,dir="data/predictorzoo"):
     #print(ppath)
     isdownloaded=check_predictors(ppath,configs['predictors_num'])
     if isdownloaded==False:  ## todo
-        download_from_url(configs['download'],ppath)
+        download_from_url(configs['download'],dir,hardware)
     
 
     ##load predictors
     predictors={}
     ps=glob(ppath+"/**.pkl")
 
-    for p in ps:
-            
+    for p in ps:            
             pname=p.split('/')[-1].replace(".pkl","")
             with open(p,'rb') as f:
                 print('load predictor',p)
@@ -28,20 +32,41 @@ def load_lat_predictors(configs,dir="data/predictorzoo"):
     return predictors,fusionrule
 
     
-def download_from_url(urladdr,ppath):
-    file_name=urladdr.split('/')[-1]
+def download_from_url(urladdr,ppath,filename):
+    file_name=ppath+"/"+'.zip'
     if os.path.isdir(ppath)==False:
         os.makedirs(ppath)
+    '''
     r = requests.get(urladdr, stream=True)
-    f = open("file_path.zip", "wb")
-
-    for chunk in r.iter_content(chunk_size=512):
-        print('here')
+    f = open(file_name, "wb")
+    for chunk in r.iter_content(chunk_size=1024):
         if chunk:
-          f.write(chunk)
+          f.write(chunk）
+    '''
+    print('download from '+urladdr)
+    response = requests.get(urladdr, stream=True)
+    total_size_in_bytes= int(response.headers.get('content-length', 0))
+    block_size = 2048 #1 Kibibyte
+    progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
+    with open(file_name, 'wb') as file:
+        for data in response.iter_content(block_size):
+            progress_bar.update(len(data))
+            file.write(data)
+    zipfile = ZipFile(file_name)
+    zipfile.extractall(path=ppath)
+    progress_bar.close()
+    os.remove(file_name)
+    #http_response = urlopen(urladdr)
+    #total_length = int(http_response.headers.get('content-length'))
+    #print('here1',total_length)
+    
+  
+   
+   
 
 
 def check_predictors(ppath,predictor_num):
+
     if os.path.isdir(ppath):
         ps=glob(ppath+"/**.pkl")
         if len(ps)!=predictor_num:
