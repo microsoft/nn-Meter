7bacf99c173224de9ad505d8e8617898a98ca898
lzhani <lzhani@microsoft.com>
2021-06-08 10:54:57 +0800

    code clean

--- nn_meter/nn_meter.py
+++ nn_meter/nn_meter.py
@@ -1,15 +1,13 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 from .prediction.predictors.predict_by_kernel import nn_predict
-from .prediction.predictors.predict_by_kernel import main_kernel_predict
 from .kerneldetection import KernelDetector
 from .ir_converters import model_to_graph, model_file_to_graph
-from .prediction.load_predictors import load_lat_predictors
+from .prediction.load_predictors import loading_to_local
 
 
 def load_latency_predictors(config):
-    kernel_predictors, fusionrule = load_lat_predictors(config['predictor'])
-
+    kernel_predictors, fusionrule = loading_to_local(config)
     nnmeter = nnMeter(kernel_predictors,fusionrule)
     return nnmeter
 
@@ -20,7 +18,7 @@ class nnMeter:
         self.fusionrule = fusionrule
         self.kd = KernelDetector(self.fusionrule)
 
-    def predict(self, model, model_type=None):
+    def predict(self, model, model_type=None,input_shape=(1, 3, 224, 224),modelname='test'):
         '''
         @params:
 
@@ -28,11 +26,12 @@ class nnMeter:
         '''
         if isinstance(model, str):
             graph = model_file_to_graph(model, model_type)
+            modelname=model.split('/')[-1].split('.')[0]
         else:
-            graph = model_to_graph(model, model_type)
+            graph = model_to_graph(model, model_type,input_shape=input_shape)
         self.kd.load_graph(graph)
-        #mid=self.__getmodelname__(model)
-        mid = "test"
-        kernel_result = {mid: self.kd.kernels}
+        
+        
+        kernel_result = {modelname: self.kd.kernels}
         py = nn_predict(self.kernel_predictors, kernel_result)
         return py
--- nn_meter/prediction/load_predictors.py
+++ nn_meter/prediction/load_predictors.py
@@ -6,19 +6,17 @@ from tqdm import tqdm
 import requests
 
 import zipfile, requests, shutil
-def load_lat_predictors(configs,dir="data/predictorzoo"):
+def loading_to_local(configs,dir="data/predictorzoo"):
     hardware=configs['name']
     ppath=dir+"/"+hardware
-    #print(ppath)
     isdownloaded=check_predictors(ppath,configs['predictors_num'])
-    if isdownloaded==False:  ## todo
+    if isdownloaded==False:  ##
         download_from_url(configs['download'],dir,hardware)
     
 
     ##load predictors
     predictors={}
     ps=glob(ppath+"/**.pkl")
-
     for p in ps:            
             pname=p.split('/')[-1].replace(".pkl","")
             with open(p,'rb') as f:
@@ -36,17 +34,11 @@ def download_from_url(urladdr,ppath,filename):
     file_name=ppath+"/"+'.zip'
     if os.path.isdir(ppath)==False:
         os.makedirs(ppath)
-    '''
-    r = requests.get(urladdr, stream=True)
-    f = open(file_name, "wb")
-    for chunk in r.iter_content(chunk_size=1024):
-        if chunk:
-          f.write(chunk）
-    '''
+    
     print('download from '+urladdr)
     response = requests.get(urladdr, stream=True)
     total_size_in_bytes= int(response.headers.get('content-length', 0))
-    block_size = 2048 #1 Kibibyte
+    block_size = 2048 #2 Kibibyte
     progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
     with open(file_name, 'wb') as file:
         for data in response.iter_content(block_size):
@@ -56,20 +48,17 @@ def download_from_url(urladdr,ppath,filename):
     zipfile.extractall(path=ppath)
     progress_bar.close()
     os.remove(file_name)
-    #http_response = urlopen(urladdr)
-    #total_length = int(http_response.headers.get('content-length'))
-    #print('here1',total_length)
-    
+   
   
    
    
 
 
-def check_predictors(ppath,predictor_num):
+def check_predictors(ppath,predictors_num):
 
     if os.path.isdir(ppath):
         ps=glob(ppath+"/**.pkl")
-        if len(ps)!=predictor_num:
+        if len(ps)!=predictors_num:
             return False
         else:
             return True
