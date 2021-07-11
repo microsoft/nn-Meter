e5db7ef3c3d20bbe3848f6b5b7b557425222b239
kalineid <nnob@mail.ustc.edu.cn>
2021-07-11 06:22:08 -0400

    Add a simple setup scripts (just for development)

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='nn_meter',
+    version='1.0',
+    description='',
+    author='',
+    author_email='',
+    url='',
+    packages=find_packages(),
+)
