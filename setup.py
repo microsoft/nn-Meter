34795e8b49c8a5e908c76a320d77c421c6a2b477
jiahangxu <jiahangxu@microsoft.com>
2021-07-27 16:39:05 +0800

    refine setup requiements

--- setup.py
+++ setup.py
@@ -16,6 +16,6 @@ setup(
         'console_scripts': ['nn-meter=nn_meter.nn_meter:nn_meter_cli'],
     },
     install_requires=[
-        'numpy', 'tqdm', 'networkx', 'requests', 'protobuf', 'PyYAML', 'scikit_learn', 'packaging', 'logging'
+        'numpy', 'tqdm', 'networkx', 'requests', 'protobuf', 'PyYAML', 'scikit_learn', 'packaging'
     ],
 )
