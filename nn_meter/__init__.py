762bc730fc04fe6da6ed79e67fd0908452378224
jiahangxu <jiahangxu@microsoft.com>
2021-07-28 15:44:48 +0800

    refactor nn-Meter init

--- nn_meter/__init__.py
+++ nn_meter/__init__.py
@@ -1,9 +1,8 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 from .nn_meter import (
-    load_latency_predictor,
     nnMeter,
-    load_predictor_config,
+    load_latency_predictor,
     list_latency_predictors,
 )
 from .utils.utils import download_from_url
