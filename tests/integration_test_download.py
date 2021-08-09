9013566c348bc72b788eb6a37a96a5a3cce3242c
jiahangxu <jiahangxu@microsoft.com>
2021-08-09 14:31:26 +0800

    add cache step 7

--- tests/integration_test_download.py
+++ tests/integration_test_download.py
@@ -2,7 +2,7 @@ import os
 from nn_meter import download_from_url
 
 if __name__ == "__main__":
-    if os.path.isdir('~/.nn_meter/cortexA76cpu_tflite21'):
+    if not os.path.isdir('~/.nn_meter/cortexA76cpu_tflite21'):
         download_from_url('https://github.com/microsoft/nn-Meter/releases/download/v1.0-data/cortexA76cpu_tflite21.zip', '~/.nn_meter')
         print("complete download in ~/.nn_meter", )
     else:
