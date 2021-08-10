761311917732e1de49ca97ab45f655c39a582d3b
jiahangxu <jiahangxu@microsoft.com>
2021-08-10 15:28:37 +0800

    add cache step 21

--- tests/integration_test.py
+++ tests/integration_test.py
@@ -95,38 +95,6 @@ def integration_test(model_type, url, ppath, output_name = "tests/test_result.tx
                 f.write(item)
 
 
-def integration_test_torch(model_type, model_list, output_name = "tests/test_result_torch.txt"):
-    """
-    download the kernel predictors from the url
-    @params:
-
-    model_type: torch
-    model_list:  the torchvision model waiting for latency prediction
-    output_name: a summary file to save the testing results
-    """
-    # if the output is not created, create it and add a title
-    if not os.path.isfile(output_name):
-        with open(output_name,"w") as f:
-            f.write('model_name, model_type, predictor, predictor_version, latency\n')
-    
-    # start testing
-    for pred_name, pred_version in get_predictors():
-        try:
-            since = time.time()
-            # print(f'nn-meter --torchvision ' + " ".join(model_list) + f' --predictor {pred_name} --predictor-version {pred_version}')
-            result = subprocess.check_output(['nn-meter', f'--torchvision'] + model_list + ['--predictor', f'{pred_name}', '--predictor-version', f'{pred_version}'])
-            runtime = time.time() - since
-        except NotImplementedError:
-            logging.error("Meets ERROR when checking --torchvision {model_string} --predictor {pred_name} --predictor-version {pred_version}")
-
-        latency_list = parse_latency_info(result.decode('utf-8'))
-        for model, latency in latency_list:
-            item = f'{model}, {model_type}, {pred_name}, {pred_version}, {round(float(latency), 4)}\n'
-            # print(item)
-            with open(output_name, "a") as f:
-                f.write(item)
-
-
 def check_getir_module(model_type, ppath):
     for model in get_models(model_type, ppath):
         try:
@@ -143,48 +111,29 @@ if __name__ == "__main__":
     check_package_status()
     output_name = "tests/test_result.txt"
 
-    since = time.time()
     # check tensorflow model
     integration_test(
         model_type='tensorflow',
-        url = "https://github.com/microsoft/nn-Meter/releases/download/v1.0-data/pb_models.zip",
-        ppath = "../data/testmodels/pb",
+        url="https://github.com/microsoft/nn-Meter/releases/download/v1.0-data/pb_models.zip",
+        ppath="../data/testmodels/pb",
         output_name=output_name
     )
-    print("tensorflow test time: ", time.time() - since)
 
-    since = time.time()
     # check onnx model
     integration_test(
         model_type='onnx',
-        url = "https://github.com/microsoft/nn-Meter/releases/download/v1.0-data/onnx_models.zip",
-        ppath = "../data/testmodels/onnx",
+        url="https://github.com/microsoft/nn-Meter/releases/download/v1.0-data/onnx_models.zip",
+        ppath="../data/testmodels/onnx",
         output_name=output_name
     )
-    print("onnx test time: ", time.time() - since)
-
-    since = time.time()
 
     # check nnmeter-ir graph model
     integration_test(
         model_type='nn-meter-ir',
-        url = "https://github.com/microsoft/nn-Meter/releases/download/v1.0-data/ir_graphs.zip",
-        ppath = "../data/testmodels/ir",
+        url="https://github.com/microsoft/nn-Meter/releases/download/v1.0-data/ir_graphs.zip",
+        ppath="../data/testmodels/ir",
         output_name=output_name
     )
-    print("nnmeter-ir test time: ", time.time() - since)
-
-    since = time.time()
-
-    # check torch model
-    integration_test_torch(
-        model_type='torch',
-        model_list=[
-            'resnet18', 'alexnet', 'vgg16', 'squeezenet', 'densenet', 'inception', 'googlenet', 
-            'shufflenet', 'mobilenet_v2', 'resnext50_32x4d', 'wide_resnet50_2', 'mnasnet'],
-        output_name=output_name
-    )
-    print("torch test time: ", time.time() - since)
 
     # check getir
     check_getir_module(
