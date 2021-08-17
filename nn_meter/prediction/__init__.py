e8cc2e848260066c2ecdb5c80bfcd50304a10038
Yuqing Yang <yuqyang@microsoft.com>
2021-08-17 12:10:22 +0800

    [doc] [code] hw-nas document & benchmark dataset code (#19)
    * add a document to brief hardware-aware model design
    
    * add benchmark dataset & remove latency_metrics from extract_feature to utils
    
    Co-authored-by: lzhani <lzhani@microsoft.com>
    Co-authored-by: Jiahang Xu <jiahang.shu@gmail.com>

--- nn_meter/prediction/__init__.py
+++ nn_meter/prediction/__init__.py
@@ -1,2 +1,4 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
+from .predictors.utils import latency_metrics
+
--- nn_meter/prediction/predictors/utils.py
+++ nn_meter/prediction/predictors/utils.py
@@ -1,6 +1,8 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
+import numpy as np
+from sklearn.metrics import mean_squared_error
 
 def get_kernel_name(optype):
     """
@@ -31,3 +33,22 @@ def get_kernel_name(optype):
         optype = "se"
 
     return optype
+
+def get_accuracy(y_pred, y_true, threshold=0.01):
+    a = (y_true - y_pred) / y_true
+    b = np.where(abs(a) <= threshold)
+    return len(b[0]) / len(y_true)
+
+
+def latency_metrics(y_pred, y_true):
+    """
+    evaluation metrics for prediction performance
+    """
+    y_true=np.array(y_true)
+    y_pred=np.array(y_pred)
+    rmspe = (np.sqrt(np.mean(np.square((y_true - y_pred) / y_true)))) * 100
+    rmse = np.sqrt(mean_squared_error(y_pred, y_true))
+    acc5 = get_accuracy(y_pred, y_true, threshold=0.05)
+    acc10 = get_accuracy(y_pred, y_true, threshold=0.10)
+    acc15 = get_accuracy(y_pred, y_true, threshold=0.15)
+    return rmse, rmspe, rmse / np.mean(y_true), acc5, acc10, acc15
