9fe548b8736b61cd6e940a976ae4d8b0b8ce6863
jiahangxu <jiahangxu@microsoft.com>
2021-08-25 17:32:17 +0800

    split torch converter in integration test

--- tests/integration_test.py
+++ tests/integration_test.py
@@ -16,22 +16,6 @@ __model_suffix__ = {
 }
 
 
-__torchvision_model_zoo__ = {
-        'resnet18': 'models.resnet18()',
-        'alexnet': 'models.alexnet()',
-        'vgg16': 'models.vgg16()',
-        'squeezenet': 'models.squeezenet1_0()',
-        'densenet161': 'models.densenet161()',
-        'inception_v3': 'models.inception_v3()',
-        'googlenet': 'models.googlenet()',
-        'shufflenet_v2': 'models.shufflenet_v2_x1_0()',
-        'mobilenet_v2': 'models.mobilenet_v2()',
-        'resnext50_32x4d': 'models.resnext50_32x4d()',
-        'wide_resnet50_2': 'models.wide_resnet50_2()',
-        'mnasnet': 'models.mnasnet1_0()',
-    }
-
-
 # check package status
 def check_package_status():
     try:
@@ -108,40 +92,7 @@ def integration_test(model_type, url, ppath, output_name = "tests/test_result.tx
             item = f'{model}, {model_type}, {pred_name}, {pred_version}, {round(float(latency), 4)}\n'
             # print(item)
             with open(output_name, "a") as f:
-                f.write(item)
-
-
-# integration test to predict model latency
-def integration_test_for_nni_based_torch(output_name = "tests/test_result_nni_based_torch.txt"):
-    """
-    download the kernel predictors from the url
-    @params:
-
-    model_type: torch
-    url: github release url address for testing model file
-    ppath:  the targeting dir to save the download model file
-    output_name: a summary file to save the testing results
-    """
-    import torchmodels as models
-    from nn_meter import load_latency_predictor
-
-    # if the output_name is not created, create it and add a title
-    if not os.path.isfile(output_name):
-        with open(output_name,"w") as f:
-            f.write('model_name, model_type, predictor, predictor_version, latency\n')
-    
-    # start testing
-    for pred_name, pred_version in get_predictors():
-        predictors = load_latency_predictor(pred_name, float(pred_version))
-        for model_name in __torchvision_model_zoo__:
-            try:
-                model = eval(__torchvision_model_zoo__[model_name])
-                latency = predictors.predict(model, "torch", apply_nni=True)
-                item = f'{model_name}, torch, {pred_name}, {pred_version}, {round(float(latency), 4)}\n'
-                with open(output_name, "a") as f:
-                    f.write(item)
-            except NotImplementedError:
-                logging.error(f"Meets ERROR when checking {model_name}")            
+                f.write(item)       
     
 
 def check_getir_module(model_type, ppath):
@@ -183,9 +134,6 @@ if __name__ == "__main__":
         ppath="../data/testmodels/ir",
         output_name=output_name
     )
-
-    # check NNI-based torch converter
-    integration_test_for_nni_based_torch()
 
     # check getir
     check_getir_module(
--- tests/integration_test_torch.py
+++ tests/integration_test_torch.py
@@ -7,10 +7,27 @@ import logging
 import subprocess
 from nn_meter import download_from_url
 from integration_test import *
+import argparse
 
 
+__torchvision_model_zoo__ = {
+        'resnet18': 'models.resnet18()',
+        'alexnet': 'models.alexnet()',
+        'vgg16': 'models.vgg16()',
+        'squeezenet': 'models.squeezenet1_0()',
+        'densenet161': 'models.densenet161()',
+        'inception_v3': 'models.inception_v3()',
+        'googlenet': 'models.googlenet()',
+        'shufflenet_v2': 'models.shufflenet_v2_x1_0()',
+        'mobilenet_v2': 'models.mobilenet_v2()',
+        'resnext50_32x4d': 'models.resnext50_32x4d()',
+        'wide_resnet50_2': 'models.wide_resnet50_2()',
+        'mnasnet': 'models.mnasnet1_0()',
+    }
+
+    
 # integration test to predict model latency
-def integration_test_torch(model_type, model_list, output_name = "tests/test_result_torch.txt"):
+def integration_test_onnx_based_torch(model_type, model_list, output_name = "tests/test_result_torch.txt"):
     """
     download the kernel predictors from the url
     @params:
@@ -41,15 +58,58 @@ def integration_test_torch(model_type, model_list, output_name = "tests/test_res
             with open(output_name, "a") as f:
                 f.write(item)
 
+
+# integration test to predict model latency
+def integration_test_nni_based_torch(output_name = "tests/test_result_nni_based_torch.txt"):
+    """
+    download the kernel predictors from the url
+    @params:
+
+    model_type: torch
+    url: github release url address for testing model file
+    ppath:  the targeting dir to save the download model file
+    output_name: a summary file to save the testing results
+    """
+    import torchmodels as models
+    from nn_meter import load_latency_predictor
+
+    # if the output_name is not created, create it and add a title
+    if not os.path.isfile(output_name):
+        with open(output_name,"w") as f:
+            f.write('model_name, model_type, predictor, predictor_version, latency\n')
+    
+    # start testing
+    for pred_name, pred_version in get_predictors():
+        predictors = load_latency_predictor(pred_name, float(pred_version))
+        for model_name in __torchvision_model_zoo__:
+            try:
+                model = eval(__torchvision_model_zoo__[model_name])
+                latency = predictors.predict(model, "torch", apply_nni=True)
+                item = f'{model_name}, torch, {pred_name}, {pred_version}, {round(float(latency), 4)}\n'
+                with open(output_name, "a") as f:
+                    f.write(item)
+            except NotImplementedError:
+                logging.error(f"Meets ERROR when checking {model_name}")     
+
+
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser('integration-test-torch')
+    parser.add_argument("--apply-onnx", help='apply onnx-based torch converter for torch model', action='store_true', default=False)
+    parser.add_argument("--apply-nni", help='apply nni-based torch converter for torch model', action='store_true', default=False)
+    args = parser.parse_args()
+
     check_package_status()
 
     # check torch model
-    integration_test_torch(
-        model_type='torch',
-        model_list=[
-            'resnet18', 'alexnet', 'vgg16', 'squeezenet', 'densenet161', 'inception_v3', 'googlenet', 
-            'shufflenet_v2', 'mobilenet_v2', 'resnext50_32x4d', 'wide_resnet50_2', 'mnasnet']
-    )
-
+    if args.apply_onnx:
+        # check ONNX-based torch converter
+        integration_test_onnx_based_torch(
+            model_type='torch',
+            model_list=[
+                'resnet18', 'alexnet', 'vgg16', 'squeezenet', 'densenet161', 'inception_v3', 'googlenet', 
+                'shufflenet_v2', 'mobilenet_v2', 'resnext50_32x4d', 'wide_resnet50_2', 'mnasnet']
+        )
+    elif args.apply_nni:
+        # check NNI-based torch converter
+        integration_test_nni_based_torch()
     
