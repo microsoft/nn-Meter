85fde5ccba360850f089a08b3e95cae50e541b62
jiahangxu <jiahangxu@microsoft.com>
2021-12-06 04:46:10 -0500

    temp stage

---
+++ nn_meter/builder/adaptive_sampler/interface.py
@@ -0,0 +1,13 @@
+
+# sampling from prior distribution
+def init_sampler():
+    pass
+
+# measure latency on device
+def measure_latency():
+    pass
+
+
+# fine-grained sampling for data with large errors
+def run_adaptive_sampler():
+    pass
