c5372a4c6162471968097bc4464b3eabfc89da96
jiahangxu <jiahangxu@microsoft.com>
2022-03-18 12:07:13 -0400

    refine conv padding config

--- nn_meter/builder/nn_generator/torch_networks/operators.py
+++ nn_meter/builder/nn_generator/torch_networks/operators.py
@@ -2,9 +2,10 @@
 # Licensed under the MIT license.
 import torch
 import torch.nn as nn
+from .utils import get_padding, get_output_shape_with_padding
 from ..interface import BaseOperator
 
-''' 
+'''
 This file contains the torch implementation of operators
 '''
 
@@ -14,7 +15,8 @@ class Conv(BaseOperator):
     def get_model(self):
         cin = self.input_shape[0]
         cout = cin if "COUT" not in self.config else self.config["COUT"]
-        return nn.Conv2d(cin, cout, kernel_size=self.config["KERNEL_SIZE"], stride=self.config["STRIDES"], padding=1)
+        padding = get_padding(self.config["KERNEL_SIZE"], self.config["STRIDES"], self.input_shape[1])
+        return nn.Conv2d(cin, cout, kernel_size=self.config["KERNEL_SIZE"], stride=self.config["STRIDES"], padding=padding)
 
     def get_output_shape(self):
         cout = self.input_shape[0] if "COUT" not in self.config else self.config["COUT"]
@@ -26,7 +28,8 @@ class Conv(BaseOperator):
 class DwConv(BaseOperator):
     def get_model(self):
         cin = self.input_shape[0]
-        return nn.Conv2d(cin, cin, kernel_size=self.config["KERNEL_SIZE"], stride=self.config["STRIDES"], padding=1, groups=cin)
+        padding = get_padding(self.config["KERNEL_SIZE"], self.config["STRIDES"], self.input_shape[1])
+        return nn.Conv2d(cin, cin, kernel_size=self.config["KERNEL_SIZE"], stride=self.config["STRIDES"], padding=padding, groups=cin)
 
     def get_output_shape(self):
         cin = self.input_shape[0]
@@ -37,13 +40,14 @@ class DwConv(BaseOperator):
 
 class ConvTrans(BaseOperator):
     def get_model(self):
+        raise NotImplementedError
         cin = self.input_shape[0]
         cout = cin if "COUT" not in self.config else self.config["COUT"]
         return nn.ConvTranspose2d(cin, cout, kernel_size=self.config["KERNEL_SIZE"], stride=self.config["STRIDES"], padding=1)
 
     def get_output_shape(self):
-        cout = self.input_shape[2] if "COUT" not in self.config else self.config["COUT"]
-        return [self.input_shape[0] * self.config["STRIDES"], self.input_shape[1] * self.config["STRIDES"], cout]
+        cout = self.input_shape[0] if "COUT" not in self.config else self.config["COUT"]
+        return [cout, self.input_shape[1] * self.config["STRIDES"], self.input_shape[2] * self.config["STRIDES"]]
 
 #------------------ normalization and pooling ------------------#
 
@@ -54,40 +58,46 @@ class BN(BaseOperator):
 
 
 class GlobalAvgpool(BaseOperator):
-    pass
+    def get_model(self):
+        raise NotImplementedError
 
 
 class MaxPool(BaseOperator):
     def get_model(self):
         if "POOL_STRIDES" not in self.config:
             self.config["POOL_STRIDES"] = self.config["STRIDES"]
-        return nn.MaxPool2d(self.config["KERNEL_SIZE"], self.config["POOL_STRIDES"], padding=1)
+        padding = get_padding(self.config["KERNEL_SIZE"], self.config["POOL_STRIDES"], self.input_shape[1])
+        return nn.MaxPool2d(self.config["KERNEL_SIZE"], self.config["POOL_STRIDES"], padding=padding)
 
     def get_output_shape(self):
+        cin = self.input_shape[0]
         if "POOL_STRIDES" not in self.config:
             self.config["POOL_STRIDES"] = self.config["STRIDES"]
         output_h = (self.input_shape[1] - 1) // self.config["POOL_STRIDES"] + 1
         output_w = (self.input_shape[2] - 1) // self.config["POOL_STRIDES"] + 1
-        return [output_h, output_w, self.input_shape[0]]
+        return [cin, output_h, output_w]
 
 
 class AvgPool(BaseOperator):
     def get_model(self):
         if "POOL_STRIDES" not in self.config:
             self.config["POOL_STRIDES"] = self.config["STRIDES"]
-        return nn.AvgPool2d(self.config["KERNEL_SIZE"], self.config["POOL_STRIDES"], padding=1)
+        padding = get_padding(self.config["KERNEL_SIZE"], self.config["POOL_STRIDES"], self.input_shape[1])
+        return nn.AvgPool2d(self.config["KERNEL_SIZE"], self.config["POOL_STRIDES"], padding=padding)
 
     def get_output_shape(self):
+        cin = self.input_shape[0]
         if "POOL_STRIDES" not in self.config:
             self.config["POOL_STRIDES"] = self.config["STRIDES"]
         output_h = (self.input_shape[1] - 1) // self.config["POOL_STRIDES"] + 1
         output_w = (self.input_shape[2] - 1) // self.config["POOL_STRIDES"] + 1
-        return [output_h, output_w, self.input_shape[0]]
+        return [cin, output_h, output_w]
 
 #------------------------ other modules ------------------------#
 
 class SE(BaseOperator):
-    pass
+    def get_model(self):
+        raise NotImplementedError
 
 
 class FC(BaseOperator):
@@ -124,26 +134,30 @@ class Hswish(BaseOperator):
 #---------------------- basic operation ----------------------#
 
 class Reshape(BaseOperator):
-    pass
+    def get_model(self):
+        raise NotImplementedError
 
 
 class Add(BaseOperator):
-    pass
+    def get_model(self):
+        raise NotImplementedError
 
 
 class Concat(BaseOperator):
-    pass
+    def get_model(self):
+        raise NotImplementedError
 
 
 class Flatten(BaseOperator):
-    pass
+    def get_model(self):
+        raise NotImplementedError
 
 
 class Split(BaseOperator):
     def get_model(self):
         cin = self.input_shape[0]
         def func(inputs):
-            return torch.split(inputs, [cin // 2, cin - cin // 2], dim=1)
+            return torch.split(inputs, [cin // 2, cin - cin // 2], dim=0)
         return func
 
     def get_output_shape(self):
