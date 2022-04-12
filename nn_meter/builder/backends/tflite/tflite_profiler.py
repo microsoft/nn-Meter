d3d876ebb41e0f0eec0020796b731194692ca94d
jiahangxu <jiahangxu@microsoft.com>
2022-04-12 02:38:36 -0400

    add xnnpack cmd

--- nn_meter/builder/backends/tflite/tflite_profiler.py
+++ nn_meter/builder/backends/tflite/tflite_profiler.py
@@ -24,7 +24,7 @@ class TFLiteProfiler(BaseProfiler):
         self._num_runs = num_runs
         self._warm_ups = warm_ups
 
-    def profile(self, graph_path, preserve=False, clean=True, taskset='70'):
+    def profile(self, graph_path, preserve = False, clean = True, taskset = '70', close_xnnpack = False):
         """
         @params:
         preserve: tflite file exists in remote dir. No need to push it again.
@@ -32,21 +32,25 @@ class TFLiteProfiler(BaseProfiler):
         """
         model_name = os.path.basename(graph_path)
         remote_graph_path = os.path.join(self._dst_graph_path, model_name)
+        taskset_cmd = f'taskset {taskset}' if taskset else ''
+        kernel_cmd = f'--kernel_path={self._dst_kernel_path}' if self._dst_kernel_path else ''
+        close_xnnpack_cmd = f'--use_xnnpack=false' if close_xnnpack else ''
 
         from ppadb.client import Client as AdbClient
         client = AdbClient(host="127.0.0.1", port=5037)
         if self._serial:
             device = client.device(self._serial)
         else:
-            device = client.devices()[0]
-
-        taskset_cmd = f'taskset {taskset}' if taskset else '' 
+            device = client.devices()[0]  
 
         if not preserve:
             device.push(graph_path, remote_graph_path)
         try:
+            taskset_cmd = f'taskset {taskset}' if taskset else '' 
             kernel_cmd = f'--kernel_path={self._dst_kernel_path}' if self._dst_kernel_path else ''
-            res = device.shell(f' {taskset_cmd} {self._benchmark_model_path} {kernel_cmd}' \
+            close_xnnpack_cmd = f'--use_xnnpack=false' if close_xnnpack else ''
+            res = device.shell(f' {taskset_cmd} {self._benchmark_model_path} {kernel_cmd} {close_xnnpack_cmd}' \
+                               f' {close_xnnpack_cmd}' \
                                f' --num_threads={self._num_threads}' \
                                f' --num_runs={self._num_runs}' \
                                f' --warmup_runs={self._warm_ups}' \
