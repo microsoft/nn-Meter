b5583fed3bd576a896a5b0feae44718cf5488ff6
jiahangxu <jiahangxu@microsoft.com>
2022-05-19 10:20:50 -0400

    add swish op

--- nn_meter/builder/nn_generator/tf_networks/operators.py
+++ nn_meter/builder/nn_generator/tf_networks/operators.py
@@ -212,6 +212,14 @@ class Hswish(BaseOperator):
             return inputs * relu6(inputs + 3.) * (1. / 6.)
         return func
 
+
+class Swish(BaseOperator):
+    # the swish op from tensorflow v2
+    def get_model(self):
+        def func(inputs):
+            return tf.keras.activations.swish(inputs)
+        return func
+
 #---------------------- basic operation ----------------------#
 
 class Reshape(BaseOperator):
