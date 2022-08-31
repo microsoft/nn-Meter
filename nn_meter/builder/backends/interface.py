87a5ae18f74e18ae3b647d3389d15d77d48998cf
jiahangxu <jiahangxu@microsoft.com>
2022-08-31 04:44:38 -0400

    change the backend profile argument

--- nn_meter/builder/backends/interface.py
+++ nn_meter/builder/backends/interface.py
@@ -70,7 +70,7 @@ class BaseBackend:
         self.parser_kwargs = {}
         self.profiler_kwargs = {}
     
-    def convert_model(self, model_path, save_path, input_shape=None):
+    def convert_model(self, model_path, save_path, input_shape = None):
         """ convert the Keras model instance to the type required by the backend inference.
 
         @params:
@@ -86,7 +86,7 @@ class BaseBackend:
         converted_model = model_path
         return converted_model
 
-    def profile(self, converted_model, metrics = ['latency'], input_shape = None, **kwargs):
+    def profile(self, converted_model, metrics = ['latency'], **kwargs):
         """
         run the model on the backend, return required metrics of the running results. nn-Meter only support latency
         for metric by now. Users may provide other metrics in their customized backend.
@@ -112,7 +112,7 @@ class BaseBackend:
             generated and used
         """
         converted_model = self.convert_model(model_path, save_path, input_shape)
-        res = self.profile(converted_model, metrics, input_shape, **kwargs)
+        res = self.profile(converted_model, metrics, input_shape=input_shape, **kwargs)
         return res
 
     def test_connection(self):
