9585afdfcef4f62b52410ab0bcf642b68887cae1
jiahangxu <jiahangxu@microsoft.com>
2022-10-27 17:53:48 +0800

    update lut predictor demo

--- examples/test_transformer/lut_predictor_demo.py
+++ examples/test_transformer/lut_predictor_demo.py
@@ -1,16 +1,14 @@
 sample = (
-    176, # input res
-    (0, 0, 0, 1, 1, 1), # block type
-    (24, 32, 64, 128, 208, 272), # channels
-    (2, 3, 2, 2, 3, 4), # depths
-    (6, 6, 6, 6, 6, 4, 4), # conv expansion ratio
-    (3, 3, 3, 3, 3, 5, 5), # conv kr size
-    (2, 2, 4, 4, 4, 4, 4, 4, 4), # trans mlp ratio
-    (14, 14, 24, 24, 24, 40, 40, 40, 40), # trans num heads
-    (1, 1, 1, 1, 1, 1, 1, 1, 1), # windows size
-    (1, 1, 1, 1, 1, 1, 1, 1, 1), # qk scale
-    (2, 2, 2, 2, 2, 2, 2, 2, 2), # v scale
-    (False, True, True, False, False, False) # use_se, only works for conv layer
+    224, # 0 input res
+    (16, 24, 40, 64, 112, 192, 320), # 1 channels
+    (1, 3, 4, 2, 3, 4, 5), # 2 depths
+    (1, 5, 5, 5, 6, 6, 6, 6), # 3 conv expansion ratio
+    (3, 5, 5, 5, 5, 5, 5, 5), # 4 conv kr size
+    (4, 4, 4, 4, 4, 3, 3, 3, 3, 3, 3, 3, 3, 3), # 5 trans mlp ratio
+    (4, 4, 7, 7, 7, 12, 12, 12, 12, 20, 20, 20, 20, 20), # 6 trans num heads
+    (1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1), # 7 windows size
+    (1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1), # 8 qk scale
+    (2, 2, 2, 2, 2, 2, 2, 2, 2, 4, 4, 4, 4, 4) # 9 v scale
 )
 
 from nn_meter.predictor.transformer_predictor import BlockLatencyPredictor
